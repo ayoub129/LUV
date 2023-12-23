ef4dd1c17234575e0d785e4101d2ca581df0c796
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2023-12-23 15:56:56 +0100

    Update sendEmail.js

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -20,6 +20,16 @@ app.use(bodyParser.json());
 
 // Define your API endpoint
 app.post('/api/sendEmail', cors(corsOptions),   (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
   const { sceneNameWithoutExtension, size, details } = req.body;
 
   // Create a transporter using nodemailer
