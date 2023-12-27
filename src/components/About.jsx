14eb34ab029798115341f1e1e93442d76c2f22bb
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2023-12-27 17:17:28 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,12 @@
 import React, { useEffect } from "react";
-import Aos from "aos";
-import "aos/dist/aos.css";
 import WebgiViewer from "./WebgiViewer";
 
 const About = () => {
-  useEffect(() => {
-    Aos.init();
-  }, []);
+
   return (
     <div className="about-container">
       <div
         className="about-text"
-        data-aos="fade-right"
-        data-aos-duration="3000"
       >
         <h2 className="title">Our Story</h2>
         <p className="phrase italicized">
@@ -21,8 +15,6 @@ const About = () => {
       </div>
       <div
         className="about-image"
-        data-aos="fade-left"
-        data-aos-duration="3000"
       >
         <WebgiViewer />
       </div>
