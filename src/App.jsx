97974472c04458949a228fd4f613dfd86e713592
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2024-01-03 15:10:10 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Home from "./screens/Home"
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+const Home = lazy(() => import('./screens/Home'));
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route index element={<Home />} />
+        <Route index element={<Suspense fallback={<div>Loading...</div>}><Home /></Suspense>} />
       </Routes>
     </Router>
   );
