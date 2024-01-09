9b35652af281682da13d4621561847998802e930
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2024-01-09 20:58:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <Route
           index
           element={
-            <Suspense fallback={<div style={{ width: '100%', height: 0, paddingBottom: '91%', position: 'relative' }}>
+            <Suspense fallback={<div style={{ width: '100%', height: 100vh, display: flex; alignItems: center; justifyContent: center; position: 'relative', backgroundColor: 'black' }}>
               <iframe src="https://giphy.com/embed/uIJBFZoOaifHf52MER" width="100%" height="100%" style={{ position: 'absolute' }} frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
             </div>}>
               <Home />
