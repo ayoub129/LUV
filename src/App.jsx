18b77cdb9a5d7b58ef19f984f16477d67a03d1b9
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2024-01-09 20:52:59 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './screens/Home';
+
+const Home = lazy(() => import('./screens/Home'));
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route index element={<Home />} />
+        <Route
+          index
+          element={
+            <Suspense fallback={<div style="width:100%;height:0;padding-bottom:91%;position:relative;"><iframe src="https://giphy.com/embed/uIJBFZoOaifHf52MER" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/UniversalMusicIndia-elvish-dg-immortals-bawli-uIJBFZoOaifHf52MER">via GIPHY</a></p>}>
+              <Home />
+            </Suspense>
+          }
+        />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
