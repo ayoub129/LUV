4efb6573fd2fd691a5431caac98573ed440d06fc
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2024-01-09 21:30:11 +0100

    Update Home.jsx

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,15 +2,17 @@ import About from "../components/About";
 import Footer from "../components/Footer";
 import Jumbotron from "../components/Jumbotron";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import {useState} from "react";
 
 const PAYPAL_CLIENT_ID = import.meta.env.VITE_REACT_APP_PAYPAL_CLIENT_ID;
 
 const Home = () => {
+  const [loading , setLoading] = useState(false)
   return (
     <PayPalScriptProvider options={{ "client-id": PAYPAL_CLIENT_ID }}>
         <div className="App">
             <Jumbotron />
-            <About />
+            <About setLoading={setLoading} />
             <Footer />
         </div>
      </PayPalScriptProvider>
